45bc5d5572a8188b6bb34e8820d2e3cf272a6c1e
蛮吉 <hanabi.almost@gmail.com>
2026-07-16 20:03:55 +0800

    chore(release): 发布 0.2.0 版本
    - 将 versionName 更新为 0.2.0
    - 将 versionCode 更新为 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "app.smartisanweather.revived"
         minSdk = 27
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     }
